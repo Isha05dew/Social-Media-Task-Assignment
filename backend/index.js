76968288f5f8eaa84940445869fb2f97cd164de0
Isha Dewangan <dewanganisha05@gmail.com>
2025-01-27 14:40:12 +0530

    undo changes

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,17 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import userRoutes from "./router/user.router.js";
-// import bodyParser from "body-parser";
-// import dotenv from 'dotenv'
-// import cors from "cors";
-
-// dotenv.config()
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// // Middleware
-// app.use(cors())
-// app.use(bodyParser.json());
-// app.use("/uploads", express.static("uploads"));
-
-// // Routes
-// app.use("/api/users", userRoutes);
-
-// // MongoDB connection
-// const connectDB = async() => {
-//     try {
-//         await mongoose.connect(process.env.MONGODB_URI)
-//         console.log('MongoDB database connected');
-//     } catch (error) {
-//         console.log('MongoDB database connection failed');
-//     }
-// }
-// // Start server
-// app.listen(PORT, () => {
-//     connectDB()
-//     console.log(`Server running on port ${PORT}`)
-// });
-
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./router/user.router.js";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
+import dotenv from 'dotenv'
 import cors from "cors";
 
-dotenv.config();
+dotenv.config()
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.json());
 app.use("/uploads", express.static("uploads"));
 
@@ -53,22 +19,16 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/users", userRoutes);
 
 // MongoDB connection
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB database connected");
-  } catch (error) {
-    console.error("MongoDB database connection failed", error);
-  }
-};
-
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log('MongoDB database connected');  
+    } catch (error) {
+        console.log('MongoDB database connection failed');
+    }
+}
 // Start server
-connectDB(); // Connect to MongoDB before listening for requests
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+app.listen(PORT, () => {
+    connectDB()
+    console.log(`Server running on port ${PORT}`)
 });
-
-export default app;
